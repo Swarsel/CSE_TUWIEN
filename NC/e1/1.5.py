650564dbd8bd56ceaef78f5a2102e4d9dd9ca5ff
Swarsele <32304731+Swarsel@users.noreply.github.com>
2022-10-17 19:14:20 +0200

    upd

--- NC/e1/1.5.py
+++ NC/e1/1.5.py
@@ -4,8 +4,8 @@ import numpy as np
 x = np.linspace(0,1.6,100)
 
 plt.plot(x, [np.sin(v) for v in x], label="$\sin(x)$")
-plt.plot(x, [v for v in x], label="$a=0$")
-plt.plot(x, [1 - 1/2 * (v - (np.pi)/2) ** 2 for v in x], label="$a= \pi/2$")
+plt.plot(x, [v for v in x], label="$a=0$", linestyle = "--")
+plt.plot(x, [1 - 1/2 * (v - (np.pi)/2) ** 2 for v in x], label="$a= \pi/2$", linestyle="--")
 
 plt.legend()
 plt.show()
