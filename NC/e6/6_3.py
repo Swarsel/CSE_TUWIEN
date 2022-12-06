68c219f24bb3b7dd5e6fcc09fb9336180c611d78
Swarsel <32304731+Swarsel@users.noreply.github.com>
2022-12-06 23:31:23 +0100

    upd

--- NC/e6/6_3.py
+++ NC/e6/6_3.py
@@ -8,8 +8,8 @@ error = [abs(np.pi - 2)]
 for k in range(1, k_max + 1):
     # sqrt goes to zero, some decimal places are lost before
     u = 2 ** k * np.sqrt(2 * (1 - np.sqrt(1 - (2 ** (-k) * u_seq[k - 1]) ** 2)))
-    print(f"k={k}, order or 2^k {np.floor(np.log10(2 ** k))} order of sqrt {np.floor(np.log10(np.sqrt(2 * (1 - np.sqrt(1 - (2 ** (-k) * u_seq[k - 1]) ** 2)))))}, error {abs(np.pi - u)}")
-    print(f"k={k}, sqrt term {np.sqrt(2 * (1 - np.sqrt(1 - (2 ** (-k) * u_seq[k - 1]) ** 2)))}")
+    print(f"k={k}, order or 2^k {np.floor(np.log10(2 ** k))}, order of sqrt {np.floor(np.log10(np.sqrt(2 * (1 - np.sqrt(1 - (2 ** (-k) * u_seq[k - 1]) ** 2)))))}, error {abs(np.pi - u)}")
+    print(f"k={k}, 2^k {2 ** k}, sqrt term {np.sqrt(2 * (1 - np.sqrt(1 - (2 ** (-k) * u_seq[k - 1]) ** 2)))}")
 
     u_seq.append(u)
 
