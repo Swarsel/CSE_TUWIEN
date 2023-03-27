d29828bb5bc84af428ff2a21a929ca6a8293822c
Swarsel <leon.schwarzaeugl@gmail.com>
2023-03-27 21:21:52 +0200

    add 3.6
    * SS2023/NumPDEs/e3/6.py:

---
+++ SS2023/NumPDEs/e3/6.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def calc_u(N,h):
+    A = np.zeros((N-1,N-1))
+    b = []
+
+    for i in range(N-1):
+        if((i-1)>=0):
+            A[i, i-1] = -1
+        A[i, i] = 2
+        if((i+1)<(N-1)):
+            A[i, i+1] = -1
+        b.append(3*(i+1)*h**3)
+    b[-1] += 1
+
+
+    #print(f"A: {A}")
+    #print(f"b: {b}")
+    u = np.linalg.solve(A,b)
+    #print(f"ui: {u}")
+    return u
+
+
+def sol(x):
+    return (-1/2)*(x**3) + (3/2)*x
+
+def error(N,h):
+    u=calc_u(N,h)
+    error = [abs(sol((i*h+(i+1)*h)/2)-((u[i]+u[i+1])/2)) for i in range(N-2)]
+    return max(error)
+
+N = 10
+h = 1/N
+
+u = calc_u(N, h)
+points = np.zeros(N-1)
+points = [i*h for i in range(1,N)]
+
+plt.subplot(211)
+plt.plot(points, [sol(x) for x in points], linestyle=":", label="exact")
+plt.plot(points, u, "x", label="approx")
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("u(x)")
+
+L = [5*i for i in range(2, 10)]
+H = [1/l for l in L]
+error = [error(n, h) for n,h in zip(L,H)]
+print(error)
+
+plt.subplot(212)
+plt.plot(L, error)
+plt.xlabel("n")
+plt.ylabel("error")
+plt.show()
