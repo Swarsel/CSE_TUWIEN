308f29c73b72b1ac1ab14a2481863c6d35aee5e7
Swarsele <32304731+Swarsel@users.noreply.github.com>
2023-04-25 18:49:13 +0200

    go

---
+++ SS2023/NumPDEs/e6/ex6_5.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def evalLegendre(x, k):
+    if k == 1:
+        return x
+    if k == 0:
+        return 1
+    else:
+        return (2 * k - 1) * x * evalLegendre(x, k - 1) - (k - 1) * evalLegendre(x, k - 2)
+
+
+def eval_integrated_legendre(x, k):
+    return (evalLegendre(x, k + 1) - evalLegendre(x, k - 1)) / (2 * k + 1)
+
+
+def gauss(f, m, k):
+    x, w = np.polynomial.legendre.leggauss(m)
+    return sum(w * f(x) * eval_integrated_legendre(x, k))
+
+
+x = np.linspace(-1, 1, 100)
+for k in range(2, 5):
+    plt.plot(x, [evalLegendre(xi, k) for xi in x], label=f"k={k}")
+plt.title("Legendre Polynomials by recursion formula")
+plt.legend()
+plt.show()
+
+for k in range(2, 5):
+    plt.plot(x, [eval_integrated_legendre(xi, k) for xi in x], label=f"k={k}")
+plt.title("Integrated Legendre Polynomials")
+plt.legend()
+plt.show()
+
+
+def sinus(x):
+    return np.sin(x)
+
+
+def cosinus(x):
+    return np.cos(x)
+
+
+def poly(x):
+    return x ** 3
+
+
+for k in range(2, 5):
+    for m in [5, 10, 15, 20]:
+        print(f"k={k}, m={m}, cos: {gauss(cosinus, m, k)}")
+        print(f"k={k}, m={m}, sin: {gauss(sinus, m, k)}")
+        print(f"k={k}, m={m}, x^3: {gauss(poly, m, k)}")
