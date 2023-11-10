a899e9e760bb55c5e8429cb8748cf853202f879f
Swarsel <leon.schwarzaeugl@gmail.com>
2023-11-10 02:16:18 +0100

    ex4 even more

--- WS2023/Many-Core Architectures/e4/1b.cu
+++ WS2023/Many-Core Architectures/e4/1b.cu
@@ -8,30 +8,30 @@
 __global__ void task1_a(int N, double *x, double *result)
 {
 
-  double sum = 0;
-  double absum = 0;
-  double sqsum = 0;
-  double nn0 = 0;
-  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < N; i += blockDim.x * gridDim.x) {
-    sum += x[i];
-    absum += abs(x[i]);
-    sqsum += pow(x[i],2);
-    if (x[i] == 0) nn0 += 1;
-  }
+    double sum = 0;
+    double absum = 0;
+    double sqsum = 0;
+    double nn0 = 0;
+    for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < N; i += blockDim.x * gridDim.x) {
+        sum += x[i];
+        absum += abs(x[i]);
+        sqsum += pow(x[i],2);
+        if (x[i] == 0) nn0 += 1;
+    }
 
-  for (int i=16; i>0; i=i/2) {
-      sum += __shfl_xor_sync(0xffffffff, sum, i);
-      absum += __shfl_xor_sync(0xffffffff, absum, i);
-      sqsum += __shfl_xor_sync(0xffffffff, sqsum, i);
-      nn0 += __shfl_xor_sync(0xffffffff, nn0, i);
-  }
+    for (int i=16; i>0; i=i/2) {
+        sum += __shfl_xor_sync(0xffffffff, sum, i);
+        absum += __shfl_xor_sync(0xffffffff, absum, i);
+        sqsum += __shfl_xor_sync(0xffffffff, sqsum, i);
+        nn0 += __shfl_xor_sync(0xffffffff, nn0, i);
+    }
 
-  if (threadIdx.x % 32 == 0) {
-      atomicAdd(&result[0], sum);
-      atomicAdd(&result[1], absum);
-      atomicAdd(&result[2], sqsum);
-      atomicAdd(&result[3], nn0);
-  }
+    if (threadIdx.x % 32 == 0) {
+        atomicAdd(&result[0], sum);
+        atomicAdd(&result[1], absum);
+        atomicAdd(&result[2], sqsum);
+        atomicAdd(&result[3], nn0);
+    }
 }
 
 
@@ -55,59 +55,59 @@ bool check(const double *result, int N) {
 int main() {
 
     int N[9] = {100, 300, 1000, 10000, 100000, 1000000, 3000000, 10000000, 100000000};
-  Timer timer;
-  double time;
-  std::cout << "[";
- //
-  // Allocate and initialize arrays on CPU
-  //
-  for(int j=0; j <= 8; j++) {
-      time = 0;
-      double *x = (double *)malloc(sizeof(double) * N[j]);
-      // double *y = (double *)malloc(sizeof(double) * N);
+    Timer timer;
+    double time;
+    std::cout << "[";
+    //
+    // Allocate and initialize arrays on CPU
+    //
+    for(int j=0; j <= 8; j++) {
+        time = 0;
+        double *x = (double *)malloc(sizeof(double) * N[j]);
+        // double *y = (double *)malloc(sizeof(double) * N);
 
-      std::fill(x, x + N[j], -2);
-      // std::fill(y, y + N, 2);
-      // for (int i = 0; i < N[j]; i++) {
-      //     if (i % 2 == 0) x[i] = -2;
-      //     else x[i] = 0;
-      // }
+        std::fill(x, x + N[j], -2);
+        // std::fill(y, y + N, 2);
+        // for (int i = 0; i < N[j]; i++) {
+        //     if (i % 2 == 0) x[i] = -2;
+        //     else x[i] = 0;
+        // }
 
-      double *cuda_x;
-      //double *cuda_y;
-      double *cuda_results;
-      CUDA_ERRCHK(cudaMalloc(&cuda_x, sizeof(double) * N[j]));
-      //CUDA_ERRCHK(cudaMalloc(&cuda_y, sizeof(double) * N));
-      CUDA_ERRCHK(cudaMalloc(&cuda_results, 4 * sizeof(double)));
+        double *cuda_x;
+        //double *cuda_y;
+        double *cuda_results;
+        CUDA_ERRCHK(cudaMalloc(&cuda_x, sizeof(double) * N[j]));
+        //CUDA_ERRCHK(cudaMalloc(&cuda_y, sizeof(double) * N));
+        CUDA_ERRCHK(cudaMalloc(&cuda_results, 4 * sizeof(double)));
 
-    for(int it=0; it < 10; it++){
-        //
-        // Allocate and initialize arrays on GPU
-        //
-        double results[4] = {0, 0, 0, 0};
+        for(int it=0; it < 10; it++){
+            //
+            // Allocate and initialize arrays on GPU
+            //
+            double results[4] = {0, 0, 0, 0};
 
-        CUDA_ERRCHK(cudaMemcpy(cuda_x, x, sizeof(double) * N[j], cudaMemcpyHostToDevice));
-        // CUDA_ERRCHK(cudaMemcpy(cuda_y, y, sizeof(double) * N, cudaMemcpyHostToDevice));
-        CUDA_ERRCHK(cudaMemcpy(cuda_results, &results, 4 * sizeof(double), cudaMemcpyHostToDevice));
-        timer.reset();
-        task1_a<<<256, 256>>>(N[j], cuda_x, cuda_results);
-        time += timer.get();
-        CUDA_ERRCHK(cudaMemcpy(&results, cuda_results, 4 * sizeof(double), cudaMemcpyDeviceToHost));
-        check(results, N[j]);
+            CUDA_ERRCHK(cudaMemcpy(cuda_x, x, sizeof(double) * N[j], cudaMemcpyHostToDevice));
+            // CUDA_ERRCHK(cudaMemcpy(cuda_y, y, sizeof(double) * N, cudaMemcpyHostToDevice));
+            CUDA_ERRCHK(cudaMemcpy(cuda_results, &results, 4 * sizeof(double), cudaMemcpyHostToDevice));
+            timer.reset();
+            task1_a<<<256, 256>>>(N[j], cuda_x, cuda_results);
+            time += timer.get();
+            CUDA_ERRCHK(cudaMemcpy(&results, cuda_results, 4 * sizeof(double), cudaMemcpyDeviceToHost));
+            check(results, N[j]);
 
-        //
-        // Clean up
-        //
-        // free(y);
+            //
+            // Clean up
+            //
+            // free(y);
+        }
+
+        if (j != 8) std::cout << time/10 << ", ";
+        // std::cout << "Result of dot product: " << results << std::endl;
+        CUDA_ERRCHK(cudaFree(cuda_x));
+        //CUDA_ERRCHK(cudaFree(cuda_y));
+        CUDA_ERRCHK(cudaFree(cuda_results));
+        free(x);
     }
-
-    if (j != 8) std::cout << time/10 << ", ";
-    // std::cout << "Result of dot product: " << results << std::endl;
-    CUDA_ERRCHK(cudaFree(cuda_x));
-    //CUDA_ERRCHK(cudaFree(cuda_y));
-    CUDA_ERRCHK(cudaFree(cuda_results));
-    free(x);
-  }
-  std::cout << time/10 << "]";
-  return EXIT_SUCCESS;
+    std::cout << time/10 << "]";
+    return EXIT_SUCCESS;
 }
--- WS2023/Many-Core Architectures/e4/1d.cu
+++ WS2023/Many-Core Architectures/e4/1d.cu
@@ -7,22 +7,22 @@
 // result = (x, y)
 __global__ void task1_d(int N, double *x, double *y, double *result)
 {
-  __shared__ double shared_mem[512];
+    __shared__ double shared_mem[256];
 
-  double dot = 0;
-  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < N; i += blockDim.x * gridDim.x) {
-    dot += x[i] * y[i];
-  }
-
-  shared_mem[threadIdx.x] = dot;
-  for (int k = blockDim.x / 2; k > 0; k /= 2) {
-    __syncthreads();
-    if (threadIdx.x < k) {
-      shared_mem[threadIdx.x] += shared_mem[threadIdx.x + k];
+    double dot = 0;
+    for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < N; i += blockDim.x * gridDim.x) {
+        dot += x[i] * y[i];
     }
-  }
 
-  if (threadIdx.x == 0) atomicAdd(result, shared_mem[0]);
+    shared_mem[threadIdx.x] = dot;
+    for (int k = blockDim.x / 2; k > 0; k /= 2) {
+        __syncthreads();
+        if (threadIdx.x < k) {
+            shared_mem[threadIdx.x] += shared_mem[threadIdx.x + k];
+        }
+    }
+
+    if (threadIdx.x == 0) atomicAdd(result, shared_mem[0]);
 }
 
 bool check(const double& result, int N) {
@@ -36,60 +36,60 @@ bool check(const double& result, int N) {
 int main() {
 
     int N[9] = {100, 300, 1000, 10000, 100000, 1000000, 3000000, 10000000, 100000000};
-  Timer timer;
-  double time;
-  std::cout << "[";
- //
-  // Allocate and initialize arrays on CPU
-  //
-  for(int j=0; j <= 8; j++) {
-      time = 0;
-      double *x = (double *)malloc(sizeof(double) * N[j]);
-      double *y = (double *)malloc(sizeof(double) * N[j]);
+    Timer timer;
+    double time;
+    std::cout << "[";
+    //
+    // Allocate and initialize arrays on CPU
+    //
+    for(int j=0; j <= 8; j++) {
+        time = 0;
+        double *x = (double *)malloc(sizeof(double) * N[j]);
+        double *y = (double *)malloc(sizeof(double) * N[j]);
 
-      std::fill(x, x + N[j], 1);
-      std::fill(y, y + N[j], 2);
-      // for (int i = 0; i < N[j]; i++) {
-      //     if (i % 2 == 0) x[i] = -2;
-      //     else x[i] = 0;
-      // }
+        std::fill(x, x + N[j], 1);
+        std::fill(y, y + N[j], 2);
+        // for (int i = 0; i < N[j]; i++) {
+        //     if (i % 2 == 0) x[i] = -2;
+        //     else x[i] = 0;
+        // }
 
-      double *cuda_x;
-      double *cuda_y;
-      double *cuda_results;
-      CUDA_ERRCHK(cudaMalloc(&cuda_x, sizeof(double) * N[j]));
-      CUDA_ERRCHK(cudaMalloc(&cuda_y, sizeof(double) * N[j]));
-      CUDA_ERRCHK(cudaMalloc(&cuda_results, sizeof(double)));
+        double *cuda_x;
+        double *cuda_y;
+        double *cuda_results;
+        CUDA_ERRCHK(cudaMalloc(&cuda_x, sizeof(double) * N[j]));
+        CUDA_ERRCHK(cudaMalloc(&cuda_y, sizeof(double) * N[j]));
+        CUDA_ERRCHK(cudaMalloc(&cuda_results, sizeof(double)));
 
-    for(int it=0; it < 10; it++){
-        //
-        // Allocate and initialize arrays on GPU
-        //
-        double results = 0;
+        for(int it=0; it < 10; it++){
+            //
+            // Allocate and initialize arrays on GPU
+            //
+            double results = 0;
 
-        CUDA_ERRCHK(cudaMemcpy(cuda_x, x, sizeof(double) * N[j], cudaMemcpyHostToDevice));
-        CUDA_ERRCHK(cudaMemcpy(cuda_y, y, sizeof(double) * N[j], cudaMemcpyHostToDevice));
-        CUDA_ERRCHK(cudaMemcpy(cuda_results, &results, sizeof(double), cudaMemcpyHostToDevice));
-        timer.reset();
-        task1_d<<<256/256, 256>>>(N[j], cuda_x, cuda_y, cuda_results);
-        time += timer.get();
-        CUDA_ERRCHK(cudaMemcpy(&results, cuda_results, sizeof(double), cudaMemcpyDeviceToHost));
-        check(results, N[j]);
+            CUDA_ERRCHK(cudaMemcpy(cuda_x, x, sizeof(double) * N[j], cudaMemcpyHostToDevice));
+            CUDA_ERRCHK(cudaMemcpy(cuda_y, y, sizeof(double) * N[j], cudaMemcpyHostToDevice));
+            CUDA_ERRCHK(cudaMemcpy(cuda_results, &results, sizeof(double), cudaMemcpyHostToDevice));
+            timer.reset();
+            task1_d<<<256, 256>>>(N[j], cuda_x, cuda_y, cuda_results);
+            time += timer.get();
+            CUDA_ERRCHK(cudaMemcpy(&results, cuda_results, sizeof(double), cudaMemcpyDeviceToHost));
+            check(results, N[j]);
 
-        //
-        // Clean up
-        //
-        // free(y);
+            //
+            // Clean up
+            //
+            // free(y);
+        }
+
+        if (j != 8) std::cout << time/10 << ", ";
+        // std::cout << "Result of dot product: " << results << std::endl;
+        CUDA_ERRCHK(cudaFree(cuda_x));
+        //CUDA_ERRCHK(cudaFree(cuda_y));
+        CUDA_ERRCHK(cudaFree(cuda_results));
+        free(x);
+        free(y);
     }
-
-    if (j != 8) std::cout << time/10 << ", ";
-    // std::cout << "Result of dot product: " << results << std::endl;
-    CUDA_ERRCHK(cudaFree(cuda_x));
-    //CUDA_ERRCHK(cudaFree(cuda_y));
-    CUDA_ERRCHK(cudaFree(cuda_results));
-    free(x);
-    free(y);
-  }
-  std::cout << time/10 << "]";
-  return EXIT_SUCCESS;
+    std::cout << time/10 << "]";
+    return EXIT_SUCCESS;
 }
