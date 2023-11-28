bf504fe0055955c76b561e913768c4e82251cf27
Swarsel <leon.schwarzaeugl@gmail.com>
2023-11-28 08:17:29 +0100

    better cuda flake

--- WS2023/Many-Core Architectures/e5/flake.nix
+++ WS2023/Many-Core Architectures/e5/flake.nix
@@ -1,30 +1,32 @@
+# flake.nix
 {
-  description = "CUDA + Python Flake for MCA";
+  description = "CUDA environment";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     llvm = pkgs.llvmPackages_latest;
   in {
     devShells.${system}.default = pkgs.mkShell {
 
-      packages = [
-
-        pkgs.gcc
+      packages = with pkgs; [
+        # gcc
         #builder
-        pkgs.cmake
-        pkgs.gnumake
+        # cmake
+        # gnumake
         #headers
-        pkgs.clang-tools
+        clang-tools
         #lsp
-        llvm.libstdcxxClang
+        # llvm.libstdcxxClang
+        # cudaPackages.cuda_nvcc
         #tools
-        pkgs.cppcheck
-        pkgs.valgrind
-        pkgs.doxygen
+        cppcheck
+        valgrind
+        doxygen
+        cudatoolkit
 
         (pkgs.python3.withPackages (python-pkgs: [
           python-pkgs.numpy
@@ -36,6 +38,9 @@
           python-pkgs.python-lsp-server
         ]))
       ];
+      hardeningDisable = ["all"];
+      # ...
+
     };
   };
 }
